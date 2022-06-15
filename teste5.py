3962da114f214a0a554887496a3760efa6d20064
Matheus carvalho lemes <85324585+matheuslemess@users.noreply.github.com>
2022-06-15 13:15:36 -0300

    first commit

---
+++ teste5.py
@@ -0,0 +1,17 @@
+def inverter(txt): 
+    return txt[::-1] 
+ 
+print(inverter('Target Sistemas')) 
+print(inverter('Target Sistemas')) 
+print(inverter('Target Sistemas')) 
+print(inverter('Target Sistemas')) 
+print(inverter('Target Sistemas')) 
+print(inverter('Target Sistemas')) 
+print(inverter('Estágio Análise e Desenvolvimento - São Paulo')) 
+print(inverter('Estágio Análise e Desenvolvimento - Ribeirão Preto')) 
+print(inverter('Target Sistemas')) 
+print(inverter('Target Sistemas')) 
+print(inverter('Target Sistemas')) 
+print(inverter('Target Sistemas')) 
+print(inverter('Target Sistemas')) 
+print(inverter('Target Sistemas')) 
